25b1bf3c17e492a7ec11e4c8fd82e177a84fd2c2
mildnomlek <kelmondlim@gmail.com>
2025-09-24 13:38:58 +0800

    Create register.js
    Test javascript for Firebase auth

---
+++ register.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyD7K7u4qUTI9C5kOr4dUe6ND6EfnxeXU-Y",
+    authDomain: "pawfectfind-cc73e.firebaseapp.com",
+    projectId: "pawfectfind-cc73e",
+    storageBucket: "pawfectfind-cc73e.firebasestorage.app",
+    messagingSenderId: "9138302822",
+    appId: "1:9138302822:web:945e4e5e46e1a7b0f7a2a7"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
